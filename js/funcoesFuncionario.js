ce9d81aa9461771c8fc9de7de2f1aac992224799
Lyncon <lynconlyn@gmail.com>
2019-04-10 17:11:25 -0300

    Select Personalizado V1

--- js/funcoesFuncionario.js
+++ js/funcoesFuncionario.js
@@ -1,28 +1,34 @@
 $(document).ready(function () {
 
-    /*$('input').focus(function(){
-        $(this).parents('.form-group').addClass('focused');
-    });
-
-    $("input").blur(function(){
-        var inputValue = $(this).val();
-        if ( inputValue == "" ) {
-            $(this).removeClass('filled');
-            $(this).parents('.form-group').removeClass('focused');
-        } else {
-            $(this).addClass('filled');
-        }
-    })*/
-
     $('input').focus(function () {
         $(this).siblings('.form-label').addClass('clicado');
         $(this).parent('.form-group').addClass('changed');
-        $(this).toggleClass('selecionado');
+
     });
 
     $('input').blur(function () {
         $(this).siblings('.form-label').removeClass('clicado');
-        $(this).toggleClass('selecionado');
         $(this).parent('.form-group').removeClass('changed');
+    });
+
+
+    $('.form-select').click(function () {
+        if($(this).parent().find('.select-option').length === 0){
+            $(this).children('.form-label').addClass('clicado');
+            $(this).addClass('changed');
+            var i = 70;
+            $('.form-select select option').each(function () {
+                $('.form-select').parent().append('<div class="select-option"></div>');
+                var op_val = $(this).val();
+                var option = $('.select-option').last();
+                option.animate({
+                    top:i+"px"
+                },{complete: function () {
+                        option.append(op_val);}
+                });
+                option.animate({width:"100%",padding: "5px"});
+                i+=70;
+            });
+        }
     });
 });
