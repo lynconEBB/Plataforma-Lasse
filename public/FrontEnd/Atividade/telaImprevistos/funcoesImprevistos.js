1c6dc7541aeafe9cd7fa1b559c4ce5efade6effa
root <lynconlyn@gmail.com>
2019-12-09 16:45:33 -0300

    Nova Estrutura Iniciada

---
+++ public/FrontEnd/Atividade/telaImprevistos/funcoesImprevistos.js
@@ -0,0 +1,245 @@
+window.onload = function () {
+    verificaMensagem();
+
+    let idUserRequisitado = window.location.pathname.split("/").pop();
+
+    requisicao("GET", "/api/atividades/user/"+idUserRequisitado, null, function (resposta, codigo) {
+        if (resposta.status === "sucesso") {
+            let requisitor = resposta.requisitor;
+
+            setLinks(requisitor);
+            if (requisitor.id === idUserRequisitado) {
+                document.getElementById("abreModalCadastro").style.display = "inline-block";
+                document.getElementById("abreModalCadastro").onclick = function (event) {
+                    exibeModal("modalCadastroImprevisto",event.target);
+                };
+                setBotaoCadastro();
+            }
+
+            if (codigo === 200) {
+
+                document.getElementById("titulo").textContent = "Imprevistos de "+resposta.dados[0].usuario.login;
+                let imprevistos = resposta.dados;
+                for (let imprevistoDados of imprevistos) {
+
+                    let imprevistoElemento = criaImprevistoContainer(imprevistoDados.id);
+                    insereDadosImprevisto(imprevistoElemento,imprevistoDados,"");
+                    setBotaoExpande(imprevistoElemento);
+                    if (requisitor.id == idUserRequisitado) {
+                        setBotoesAcoes(imprevistoElemento,imprevistoDados);
+                    }
+                }
+            } else {
+                requisicao("GET","/api/users/"+idUserRequisitado,null,function (resposta,codigo) {
+                    if (resposta.status === "sucesso") {
+                        document.getElementById("titulo").textContent = "Imprevistos de "+resposta.dados.login;
+                    }
+                });
+                document.getElementsByClassName("imprevistos-body")[0].innerHTML = `
+                    <div class="erro-container">
+                        <figure>
+                            <img alt="Icone de Vazio" src="/public/FrontEnd/images/vazio.png">
+                        </figure>
+                        <h2 class="erro-texto">Nenhum Imprevisto cadastrado por este usuário</h2>
+                    </div>
+                `;
+            }
+        } else {
+            decideErros(resposta, codigo);
+        }
+    });
+};
+
+function criaImprevistoContainer(IdImprevisto) {
+    let container = document.getElementsByClassName("imprevistos-body")[0];
+    container.insertAdjacentHTML("beforeend", `
+        <div class="imprevisto" aria-expanded="false" id="imprevisto${IdImprevisto}" tabindex="-1"></div>
+    `);
+    return document.getElementById("imprevisto"+IdImprevisto);
+}
+
+function insereDadosImprevisto(imprevistoElemento,imprevisto,expandido) {
+    imprevistoElemento.innerHTML = `
+        <div class="imprevisto-info">
+            <span><span class="escondeVisualmente">Tipo:</span> ${imprevisto.tipo}</span>
+            <span><span class="escondeVisualmente">Data: </span> ${imprevisto.dataRealizacao}</span>
+            <span class="total"><span class="escondeVisualmente">Total Gasto: </span> R$ ${imprevisto.totalGasto}</span>
+            <div class="imprevisto-detalhes ${expandido}">
+                <div class="detalhe-group">
+                    <span class="detalhe-nome">Comentario</span>
+                    <span class="detalhe-valor">${imprevisto.comentario}</span>
+                </div>
+                <div class="detalhe-group">
+                    <span class="detalhe-nome">Tempo Gasto</span>
+                    <span class="detalhe-valor">${imprevisto.tempoGasto}</span>
+                </div>
+                <div class="detalhe-group total">
+                    <span class="detalhe-nome">Total Gasto</span>
+                    <span class="detalhe-valor">R$ ${imprevisto.totalGasto}</span>
+                </div>
+            </div>
+        </div>
+        <button class="expande" aria-label="Expande detalhes do Imprevisto ${imprevisto.tipo} ocorrido em ${imprevisto.dataRealizacao} com total gasto de R$ ${imprevisto.totalGasto}">
+              <i class="material-icons">keyboard_arrow_down</i>          
+        </button>
+    `;
+}
+
+function setBotaoExpande(imprevisto) {
+    let imprevistoDetalhes = imprevisto.querySelector(".imprevisto-detalhes");
+    let btnExpande =  imprevisto.querySelector(".expande");
+
+    if (imprevistoDetalhes.classList.contains("expandido")) {
+        btnExpande.innerHTML = "<i class='material-icons'>keyboard_arrow_up</i> ";
+    } else {
+        btnExpande.innerHTML = "<i class='material-icons'>keyboard_arrow_down</i> ";
+    }
+
+    imprevisto.querySelector(".expande").onclick = function () {
+        let label = btnExpande.getAttribute("aria-label");
+
+        if (imprevistoDetalhes.classList.contains("expandido")) {
+            imprevistoDetalhes.classList.remove("expandido");
+            let newLabel = label.replace("Reduz","Expande");
+            btnExpande.setAttribute("aria-label",newLabel);
+            btnExpande.innerHTML = "<i class='material-icons'>keyboard_arrow_down</i> ";
+        } else {
+            imprevistoDetalhes.classList.add("expandido");
+            let newLabel = label.replace("Expande","Reduz");
+            btnExpande.setAttribute("aria-label",newLabel);
+            imprevisto.focus();
+            btnExpande.innerHTML = "<i class='material-icons'>keyboard_arrow_up</i> ";
+        }
+    };
+}
+
+function setBotoesAcoes(imprevisto,imprevistoDados) {
+
+    let imprevistoDetalhes = imprevisto.querySelector(".imprevisto-detalhes");
+
+    imprevistoDetalhes.insertAdjacentHTML("beforeend",`
+        <div class="detalhe-botoes">
+            <button class="botao info btn-detalhe">Alterar</button>
+            <button class="botao alerta btn-detalhe">Excluir</button>
+        </div>
+    `);
+
+    let botaoExibeInputs = imprevistoDetalhes.querySelector(".info");
+    botaoExibeInputs.onclick = function () {
+        exibeInputs(imprevisto,imprevistoDados);
+        setBotoesAlterar(imprevisto,imprevistoDados);
+        imprevisto.querySelector("#tipo").focus();
+    };
+
+    let botaoExibeModalExcluir = imprevistoDetalhes.querySelector(".alerta");
+    botaoExibeModalExcluir.onclick =  function (event) {
+        requisicao("DELETE","/api/atividades/"+imprevistoDados.id,null,function (resposta,codigo) {
+            if (resposta.status === "sucesso") {
+                addMensagem("sucesso=Imprevisto-excluido-com-sucesso");
+            } else {
+                exibirMensagem(resposta.mensagem,true,event.target);
+            }
+        });
+    };
+}
+
+function exibeInputs(imprevistoElemento,imprevisto) {
+
+    let imprevistoInfo = imprevistoElemento.querySelector(".imprevisto-info");
+
+    imprevistoInfo.innerHTML = `
+        <div class="imprevisto-group">
+            <label class="escondeVisualmente" for="tipo">Tipo</label>
+            <select class="alterar-select" id="tipo">
+                <option value="Reunião Interna">Reunião Interna</option>
+                <option value="Reunião Externa">Reunião Externa</option>
+                <option value="Atividade Acadêmica">Atividade Acadêmica</option>
+                <option value="Workshop Interno">Workshop Interno</option>
+                <option value="Workshop Externo">Workshop Externo</option>
+                <option value="Trabalho em Campo">Trabalho em Campo</option>
+                <option value="Operacional">Operacional</option>
+                <option value="Treinamento Externo">Treinamento Externo</option>
+                <option value="Orientação">Orientação</option>
+                <option value="Compensação">Compensação</option>
+                <option value="Projeto Interno">Projeto Interno</option>
+                <option value="Atestado Médico">Atestado Médico</option>
+            </select>
+        </div>
+        <div class="imprevisto-group" >
+            <label class="escondeVisualmente" for="data">Data de Realização</label>
+            <input class="alterar-input" id="data" type="text" value="${imprevisto.dataRealizacao}">
+        </div>
+        
+        <span class="total"><span class="escondeVisualmente">Total Gasto: </span> R$ ${imprevisto.totalGasto}</span>
+        
+        <div class="imprevisto-detalhes expandido">
+            <div class="detalhe-group">
+                <label class="alterar-label" for="comentario">Comentário</label>
+                <textarea class="alterar-area" id="comentario" rows="3">${imprevisto.comentario}</textarea>
+            </div>
+            <div class="detalhe-group">
+                <label class="alterar-label" for="tempoGasto">Tempo Gasto</label>
+                <input class="alterar-input" type="text" id="tempoGasto" value="${imprevisto.tempoGasto}">
+            </div>
+            <div class="detalhe-group total">
+                <span class="detalhe-nome">Total Gasto</span>
+                <span class="detalhe-valor">R$ ${imprevisto.totalGasto}</span>
+            </div>
+            <div class="detalhe-botoes">
+                <button class="botao alerta btn-detalhe">Cancelar</button>
+                <button class="botao sucesso btn-detalhe">Salvar</button>
+            </div>
+        </div>`;
+    imprevistoInfo.querySelector("#tipo").value = imprevisto.tipo;
+    setBotaoExpande(imprevistoElemento);
+    return imprevistoInfo;
+}
+
+function setBotoesAlterar(imprevisto,imprevistoDados) {
+    let botaoCancelar  = imprevisto.querySelector(".alerta");
+
+    botaoCancelar.onclick = function () {
+        insereDadosImprevisto(imprevisto,imprevistoDados,"expandido");
+        setBotaoExpande(imprevisto);
+        setBotoesAcoes(imprevisto,imprevistoDados);
+    };
+
+    imprevisto.querySelector(".sucesso").onclick =  function (event) {
+        let body = {
+            tipo: imprevisto.querySelector("#tipo").value,
+            tempoGasto: imprevisto.querySelector("#tempoGasto").value,
+            comentario: imprevisto.querySelector("#comentario").value,
+            dataRealizacao: imprevisto.querySelector("#data").value,
+        };
+
+        requisicao("PUT", "/api/atividades/"+imprevistoDados.id, body, function (resposta, codigo) {
+            if (resposta.status === "sucesso") {
+                addMensagem("sucesso=Imprevisto-alterado-com-sucesso");
+            } else {
+                exibirMensagem(resposta.mensagem, true, event.target);
+            }
+        });
+    }
+}
+
+function setBotaoCadastro() {
+    let botaoCadastro = document.getElementById("cadastraImprevisto");
+    botaoCadastro.onclick = function (event) {
+        event.preventDefault();
+        let body = {
+            tipo: document.querySelector("#tipoCadastro").value,
+            tempoGasto: document.querySelector("#tempoCadastro").value,
+            comentario: document.querySelector("#comentarioCadastro").value,
+            dataRealizacao: document.querySelector("#dataCadastro").value,
+        };
+
+        requisicao("POST","/api/atividades/",body,function (resposta,codigo) {
+            if (resposta.status === "sucesso") {
+                addMensagem("sucesso=Imprevisto-cadastrado-com-sucesso");
+            } else {
+                exibirMensagem(resposta.mensagem,true,event.target);
+            }
+        });
+    }
+}
+
---
+++ public/FrontEnd/Default/js/funcoesPadrao.js
@@ -0,0 +1,163 @@
+function setLinks(requisitor) {
+    let idUsuario = requisitor.id;
+    let btnProjetos = document.querySelector("#projetos");
+    btnProjetos.href = "/projetos/user/"+idUsuario;
+    let btnGraficos = document.querySelector("#graficos");
+    btnGraficos.href = "/graficos/user/"+idUsuario;
+    let btnFormularios = document.querySelector("#formularios");
+    btnFormularios.href = "/formularios/user/"+idUsuario;
+    let btnPerfil = document.querySelector("#perfil");
+    btnPerfil.href = "/perfil/user/"+idUsuario;
+    let btnDashboard = document.querySelector("#dashboards");
+    btnDashboard.href = "/dashboard/user/"+idUsuario;
+    let btnImprevistos = document.querySelector("#imprevistos");
+    btnImprevistos.href = "/imprevistos/user/"+idUsuario;
+    let btnAjuda= document.querySelector("#ajuda");
+    btnAjuda.href = "/ajuda/"+idUsuario;
+
+    document.querySelector(".container-user-img").style.backgroundImage = "url('/"+requisitor.foto+"')";
+    document.querySelector(".user-name").textContent = requisitor.login;
+
+    if (requisitor.admin === "1") {
+        document.querySelector(".container-itens-admin").style.display = "block";
+        document.getElementById("graficosAdmin").href = "/admin/graficos";
+        document.getElementById("todosProjetos").href = "/projetos/user/todos";
+        document.getElementById("usuarios").href = "/users";
+    }
+}
+
+function verificaMensagem() {
+    if (window.performance) {
+        if (window.performance.navigation.type === 0) {
+            criaMensagemURL();
+        }
+    } else {
+        criaMensagemURL();
+    }
+}
+
+function criaMensagemURL() {
+    let url = window.location.href;
+
+    if (url.indexOf("?") !== -1) {
+        let variavel = url.substring(url.indexOf("?")+1,url.length);
+        let partes = variavel.split("=");
+        if (partes.length === 2 && partes[0] === "sucesso") {
+            let mensagem = partes[1].replace(/-/g," ");
+            exibirMensagemInicio(mensagem,false);
+        } else if (partes.length === 2 && partes[0] === "erro") {
+            let mensagem = partes[1].replace(/-/g," ");
+            exibirMensagemInicio(mensagem,true,null);
+        }
+    }
+}
+
+function addMensagem(mensagem) {
+    let url = location.href;
+    if (url.indexOf("?") !== -1) {
+        url = url.substring(0,url.indexOf("?"));
+    }
+    url = url+"?"+mensagem;
+    window.location.href = url;
+}
+
+function decideErros(resposta,codigo) {
+    let erro = resposta.dados;
+
+    if (codigo === 405) {
+        window.location.href = "/?erro=Logue-no-sistema-para-ter-acesso!";
+    }
+    else if (codigo === 401) {
+        setLinks(resposta.requistor);
+        document.getElementsByClassName("main-content")[0].innerHTML = criaTemplateSemPermissao(erro);
+    }
+    else if (codigo === 404) {
+        setLinks(resposta.requistor);
+        document.getElementsByClassName("main-content")[0].innerHTML = criaTemplateNaoEncontrado(erro);
+    }
+    else if (codigo === 500) {
+        if (resposta.hasOwnProperty("requisitor")) {
+            setLinks(resposta.requistor);
+        }
+        document.getElementsByClassName("main-content")[0].innerHTML = criaTemplateErroInterno(erro);
+    }
+}
+
+function requisicao(metodo,url,body,response) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(metodo, url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    if (body !== null) {
+        xhr.send(JSON.stringify(body));
+    } else {
+        xhr.send();
+    }
+
+    xhr.onload = function() {
+        console.log(xhr.response);
+        response(JSON.parse(xhr.response),xhr.status);
+    };
+
+    xhr.onerror = function () {
+        exibirMensagem("Servidor não respondendo",true);
+    };
+}
+
+function criaTemplateNaoEncontrado(erro) {
+    let templateErroNaoEncontrado;
+    return templateErroNaoEncontrado = `
+        <div class="container-erro">
+            <figure class="container-img-erro">
+                <img alt="Icone indicando um erro de que algo não foi encontrado. Ilustração" src="/public/FrontEnd/images/mascote/RaioSozinho.png">
+            </figure>
+
+            <h1>Ops! Não encontramos o que você estava procurando</h1>
+            <h2>${erro.mensagem}</h2>
+        </div>
+    `;
+}
+
+function criaTemplateSemPermissao(erro) {
+    return templateErroPermisso = `
+        <div class="container-erro">
+            <figure class="container-img-erro-permissao">
+                <img alt="Icone indicando um erro de que o usuário não possui acesso à está página. Ilustração" src="/public/FrontEnd/images/mascote/RaioBarrado.png">
+            </figure>
+
+            <h1>Parece que você está entrando em lugares que não deveria!</h1>
+            <h2>${erro.mensagem}</h2>
+        </div>
+`;
+}
+
+function criaTemplateErroInterno(erro) {
+    return templateErroInterno = `
+        <div class="container-erro">
+            <figure class="container-img-erro">
+                <img alt="Icone indicando um erro de que o usuário não possui acesso à está página. Ilustração" src="/public/FrontEnd/images/mascote/RaioDuvida.png">
+            </figure>
+
+            <h1>Opa! Parece que houve um erro inesperado em nossos servidores</h1>
+            <h2>Tente novamente mais tarde</h2>
+        </div>
+`;
+}
+
+/*window.onbeforeunload = function () {
+    if (window.performance) {
+        if (window.performance.navigation.type !== 1 ) {
+            let cookie = document.cookie;
+            if (cookie !== "") {
+                deslogar(cookie);
+            }
+        }
+    }
+};*/
+
+function deslogar() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("DELETE","/api/users/deslogar");
+    xhr.send();
+}
+
